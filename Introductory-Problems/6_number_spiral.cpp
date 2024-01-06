bde3f68e42bebb3074d1b7771db1282d98efe45e
Ansh <gujralanshg@gmail.com>
2024-01-07 00:12:05 +0530

    problem-6

---
+++ Introductory-Problems/6_number_spiral.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void spiral(ll row, ll col)
+{
+    ll result = 0;
+
+    if (col - row > 0)
+    {
+        result += (col & 1 == 1) ? col * col : (col - 1) * (col - 1) + 1;
+        bool decrease = !(col & 1 == 1);
+        for (int i = row; i > 1; i--)
+        {
+            result -= (decrease) ? -1 : 1;
+        }
+    }
+    else
+    {
+        result += (row & 1 == 1) ? (row - 1) * (row - 1) + 1 : row * row;
+        bool decrease = !(row & 1 == 1);
+        for (int i = col; i > 1; i--)
+        {
+            result -= (decrease) ? 1 : -1;
+        }
+    }
+    cout << result << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int
+            ll temp1,
+            temp2;
+        cin >> temp1;
+        cin >> temp2;
+        spiral(temp1, temp2);
+    }
+}
